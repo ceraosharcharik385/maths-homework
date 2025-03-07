81079ac7526bd8298ef576652ed56d302bd2da7b
ceraosharcharik385 <ceraosharcharik385@gmail.com>
2025-03-07 01:10:19 +0000

    Add 3.py

---
+++ src/3.py
@@ -0,0 +1,7 @@
+from random import randint
+
+def get_random_code():
+    # Generate a random number between 1 and 10
+    num = randint(1, 10)
+    # Return the random number as a string
+    return str(num)
