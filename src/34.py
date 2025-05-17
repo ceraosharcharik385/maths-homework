81837a67c51bb3fb76148231ffa4169ae6984a80
ceraosharcharik385 <ceraosharcharik385@gmail.com>
2025-05-17 04:13:16 +0000

    Add 34.py

---
+++ src/34.py
@@ -0,0 +1,9 @@
+import math
+import random
+
+def calculate_area_rectangle(length, width):
+    area = length * width
+    return area
+
+if __name__ == "__main__":
+    print("Rectangle area:", calculate_area_rectangle(10, 5))
